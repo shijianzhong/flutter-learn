eff2835cd27eb22767feb10181d48e6921704fbb
史建忠 <994129509@qq.com>
2019-07-17 09:28:31 +0800

    2019年07月17日09:28:30

---
+++ demo01/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'views/login/login.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Login(),
+    );
+  }  
+}
+
---
+++ demo01/lib/views/login/login.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  var _isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: Text('登录')),
+        body: Container(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Center(
+                  child: Column(
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('images/logo.png'),
+                    height: 200.0,
+                  ),
+                  Text(
+                    '机关项目',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              )),
+              Row(
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                  )
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(20.0),
+                    hintText: '用户名',
+                    icon: Icon(Icons.person)),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    height: 20.0,
+                  )
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20.0),
+                  icon: Icon(Icons.lock),
+                  hintText: '密码',
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Checkbox(
+                    value: _isChecked,
+                    onChanged: (bool bl) {
+                      if (mounted) {
+                        setState(() {
+                          _isChecked = bl;
+                        });
+                      }
+                    },
+                  ),
+                  Text('记住密码')
+                ],
+              ),
+              RaisedButton(
+                child: Text('登录'),
+                textColor: Colors.white,
+                onPressed: _login,
+              ),
+            ],
+          ),
+        ));
+  }
+
+  void _login() {}
+}
